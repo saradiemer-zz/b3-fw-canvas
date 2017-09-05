887d44ba1cc6f87cf051ec38fa733626aa95476e
sravenscraft <sravenscraft@luc.edu>
2017-09-05 14:00:11 -0500

    Fix to mobile nav and display on IE

--- gulp-tasks/t4-media.js
+++ gulp-tasks/t4-media.js
@@ -11,6 +11,7 @@ gulp.task('t4_media', function() {
 	return gulp.src(config.publicDir + '/T4*.html')
 	//change references	in html
 	.pipe(replace('/css/styles.css', '<t4 type="media" id="472265" formatter="path/*"/>'))
+  .pipe(replace('all-ie-only.css', '<t4 type="media" id="476101" formatter="path/*"/>'))
 	.pipe(replace('/js/all.min.js', '<t4 type="media" id="466247" formatter="path/*"/>'))
 	.pipe(replace('<script src="/js/all.js"></script>', ''))
   .pipe(replace('/images/homepage/home-campus-aerial-500-500.jpg', '<t4 type="media" id="474881" formatter="path/*"/>'))
